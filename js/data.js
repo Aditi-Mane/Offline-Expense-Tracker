7de35cfdf0d7e2c21cf3b41689c25b03b40b55ce
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2026-01-09 22:26:00 +0530

    feat: add localStorage persistence for transactions and friend transactions

--- js/data.js
+++ js/data.js
@@ -1,3 +1,43 @@
 let transactions = [];
 
 let friendTransactions = [];
+
+//function to save data in localStorage
+function saveTransactions() {
+  localStorage.setItem(
+    "transactions",
+    JSON.stringify(transactions)
+  );
+}
+
+//function to load data from localStorage
+function loadTransactions(){
+  const storage = localStorage.getItem("transactions");
+
+  if(storage){
+    //conversion of string from local storage back to object format
+    transactions = JSON.parse(storage); 
+  } else {
+    transactions = [];
+  }
+}
+
+function saveFriendTransactions() {
+  localStorage.setItem(
+    "friendTransactions",
+    JSON.stringify(friendTransactions)
+  );
+}
+
+function loadFriendTransactions(){
+  const storage = localStorage.getItem("friendTransactions");
+
+  if(storage){
+    //conversion of string from local storage back to object format
+    friendTransactions = JSON.parse(storage); 
+  } else {
+    friendTransactions = [];
+  }
+}
+
+
