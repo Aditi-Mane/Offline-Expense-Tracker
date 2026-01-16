c3033ceb04d9eb49075854762d7126138fbbd97c
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2026-01-16 17:46:50 +0530

    feat: add dark mode

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded",()=>{
   updateSummary();
   updateFriendSummary();
 })
+
+const darkBtn = document.getElementById("dark-mode");
+const lightBtn = document.getElementById("light-mode");
+
+darkBtn.addEventListener("click",()=>{
+  document.body.classList.add("dark");
+  localStorage.setItem("theme", "dark");
+})
+lightBtn.addEventListener("click",()=>{
+  document.body.classList.remove("dark");
+  localStorage.setItem("theme", "light");
+})
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+  document.body.classList.add("dark");
+}
